13473531da54c894b784283e8d24b7074e5ab5a4
Binary44 <binarycoder.com@gmail.com>
2022-09-02 20:20:27 +0100

    assects

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>MY SECOND REACT NATIVE APPLICATION!</Text>
       <StatusBar style="auto" />
     </View>
   );
